32f6621c844a50a127dc61f0562caaee56e7722f
MarioLara21 <62675263+MarioLara21@users.noreply.github.com>
2020-08-27 23:40:00 -0600

    Sección 2
    Esto es lo que logré copiar de las clases en la sección 2 del curso.

---
+++ Comentarios.py
@@ -0,0 +1,25 @@
+#Creado por Mario Lara Molina
+#Fecha de creación: 27/08/2020
+#Última modificación: 27/08/2020
+#Versión 3.8.3
+
+#Los comentarios los hago con la almohadilla
+#Sirven para dejar anotaciones para el futuro
+#Todo esto el intérprete lo ignora y sigue su curso
+print("Hola")
+print("Mundo")
+# print("!!")
+
+"""
+Con tres comillas simples o dobles podemos
+hacer comentarios largos sin tener que
+poner varias almohadillas.
+"""
+
+"""
+Esto no se mostrará:
+    print("Esto no se va a mostrar")
+    print("Porque estará dentro de un comentario multilínea")
+"""
+
+print("Adios")
